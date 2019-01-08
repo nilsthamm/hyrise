d10aab2da34ae20b65002369d8adbbdcea59215c
philipp.bode <philipp.bode@student.hpi.de>
2019-01-08 22:02:23 +0100

    Move ConcatenatedConstraintChecker to simple function.

--- src/lib/storage/constraints/unique_checker.cpp
+++ src/lib/storage/constraints/unique_checker.cpp
@@ -9,14 +9,62 @@ bool check_constraint(std::shared_ptr<const Table> table, const TableConstraintD
   return false;
 }
 
-bool check_constraint(std::shared_ptr<const Table> table, const TableConstraintDefinition& constraint, const CommitID& last_commit_id, const TransactionID& our_tid) {
-  const auto constraint_checker = std::make_shared<ConcatenatedConstraintChecker>(table, constraint, last_commit_id, our_tid);
-  return constraint_checker->check();
+bool check_constraint(std::shared_ptr<const Table> table, const TableConstraintDefinition& constraint, const CommitID& snapshot_commit_id, const TransactionID& our_tid) {
+  if (constraint.is_primary_key) {
+    for (const auto& column_id : constraint.columns) {
+      if (table->column_is_nullable(column_id)) {
+        return false;
+      }
+    }
+  }
+
+  std::set<boost::container::small_vector<AllTypeVariant, 3>> unique_values;
+
+  for (const auto& chunk : table->chunks()) {
+    const auto mvcc_data = chunk->get_scoped_mvcc_data_lock();
+
+    const auto& segments = chunk->segments();
+    for (ChunkOffset chunk_offset = 0; chunk_offset < chunk->size(); chunk_offset++) {
+      const auto row_tid = mvcc_data->tids[chunk_offset].load();
+      const auto begin_cid = mvcc_data->begin_cids[chunk_offset];
+      const auto end_cid = mvcc_data->end_cids[chunk_offset];
+      auto row = boost::container::small_vector<AllTypeVariant, 3>();
+      row.reserve(constraint.columns.size());
+      bool row_contains_null = false;
+
+      if (!Validate::is_row_visible(our_tid, snapshot_commit_id, row_tid, begin_cid, end_cid)) {
+        continue;
+      }
+      for (const auto& column_id : constraint.columns) {
+        const auto& segment = segments[column_id];
+        const auto& value = segment->operator[](chunk_offset);
+        if (variant_is_null(value)) {
+          row_contains_null = true;
+        }
+        row.emplace_back(value);
+      }
+
+      // this handles null values in unique constraints as follows:
+      // - if a row doesn't contain any null values, the tuple of the columns must be unique
+      // - if a row contains any null values:
+      //   - there may be multiple tuples with same values
+      //   - because a null value could be any value, these tuples are treated as unique
+      //   - so we don't have to put anything into the set of unique values
+      if (row_contains_null) {
+        continue;
+      }
+      const auto& [iterator, inserted] = unique_values.insert(row);
+      if (!inserted) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
-bool check_constraints(std::shared_ptr<const Table> table, const CommitID& last_commit_id, const TransactionID& our_tid) {
+bool check_constraints(std::shared_ptr<const Table> table, const CommitID& snapshot_commit_id, const TransactionID& our_tid) {
   for (const auto& constraint : table->get_unique_constraints()) {
-    if (!check_constraint(table, constraint, last_commit_id, our_tid)) {
+    if (!check_constraint(table, constraint, snapshot_commit_id, our_tid)) {
       return false;
     }
   }
@@ -32,10 +80,10 @@ bool check_constraints(std::shared_ptr<const Table> table) {
   return true;
 }
 
-bool check_constraints(const std::string &table_name, const CommitID& last_commit_id, const TransactionID& our_tid) {
+bool check_constraints(const std::string &table_name, const CommitID& snapshot_commit_id, const TransactionID& our_tid) {
   auto const table = StorageManager::get().get_table(table_name);
   for (const auto& constraint : table->get_unique_constraints()) {
-    if (!check_constraint(table, constraint, last_commit_id, our_tid)) {
+    if (!check_constraint(table, constraint, snapshot_commit_id, our_tid)) {
       return false;
     }
   }
--- src/lib/storage/constraints/unique_checker.hpp
+++ src/lib/storage/constraints/unique_checker.hpp
@@ -58,81 +58,9 @@ class UniqueConstraintChecker : public BaseConstraintChecker {
   }
 };
 
-
-// New ConstraintChecker that should use vectors of AllTypeVariants to allow concatenated constraints.
-class ConcatenatedConstraintChecker {
- public:
-  ~ConcatenatedConstraintChecker() = default;
-
-  ConcatenatedConstraintChecker(
-    const std::shared_ptr<const Table> table,
-    const TableConstraintDefinition& constraint,
-    const CommitID& snapshot_commit_id,
-    const TransactionID& our_tid ) : _table(table), _constraint(constraint), _snapshot_commit_id(snapshot_commit_id), _our_tid(our_tid) {}
-
-  bool check() const {
-    if (_constraint.is_primary_key) {
-      for (const auto& column_id : _constraint.columns) {
-        if (_table->column_is_nullable(column_id)) {
-          return false;
-        }
-      }
-    }
-
-    std::set<boost::container::small_vector<AllTypeVariant, 3>> unique_values;
-
-    for (const auto& chunk : _table->chunks()) {
-      const auto mvcc_data = chunk->get_scoped_mvcc_data_lock();
-
-      const auto& segments = chunk->segments();
-      for (ChunkOffset chunk_offset = 0; chunk_offset < chunk->size(); chunk_offset++) {
-        const auto row_tid = mvcc_data->tids[chunk_offset].load();
-        const auto begin_cid = mvcc_data->begin_cids[chunk_offset];
-        const auto end_cid = mvcc_data->end_cids[chunk_offset];
-        auto row = boost::container::small_vector<AllTypeVariant, 3>();
-        row.reserve(_constraint.columns.size());
-        bool row_contains_null = false;
-
-        if (!Validate::is_row_visible(_our_tid, _snapshot_commit_id, row_tid, begin_cid, end_cid)) {
-          continue;
-        }
-        for (const auto& column_id : _constraint.columns) {
-          const auto& segment = segments[column_id];
-          const auto& value = segment->operator[](chunk_offset);
-          if (variant_is_null(value)) {
-            row_contains_null = true;
-          }
-          row.emplace_back(value);
-        }
-
-        // this handles null values in unique constraints as follows:
-        // - if a row doesn't contain any null values, the tuple of the columns must be unique
-        // - if a row contains any null values:
-        //   - there may be multiple tuples with same values
-        //   - because a null value could be any value, these tuples are treated as unique
-        //   - so we don't have to put anything into the set of unique values
-        if (row_contains_null) {
-          continue;
-        }
-        const auto& [iterator, inserted] = unique_values.insert(row);
-        if (!inserted) {
-          return false;
-        }
-      }
-    }
-    return true;
-  }
-
- protected:
-  std::shared_ptr<const Table> _table;
-  TableConstraintDefinition _constraint;
-  CommitID _snapshot_commit_id;
-  TransactionID _our_tid;
-};
-
 bool check_constraint(std::shared_ptr<const Table> table, const TableConstraintDefinition& constraint);
 bool check_constraints(std::shared_ptr<const Table> table);
-bool check_constraints(std::shared_ptr<const Table> table, const CommitID& last_commit_id, const TransactionID& our_tid);
+bool check_constraints(std::shared_ptr<const Table> table, const CommitID& snapshot_commit_id, const TransactionID& our_tid);
 bool check_constraints(const std::string &table, const CommitID& snapshot_commit_id, const TransactionID& our_tid);
 
 }  // namespace opossum
